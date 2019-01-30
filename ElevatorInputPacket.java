f8b7e766c805d36d86cc80b09ada6a14c76c90a6
csworthington <craigworthington33@gmail.com>
2019-01-30 15:29:31 -0500

    Created a new ElevatorDatagramPacket
    Created new ElevatorDatagramPacket class to mimic the built in DatagramPacket class. Will be used to send and receive information between scheduler, elevator and floors

--- ElevatorInputPacket.java
+++ ElevatorInputPacket.java
@@ -1,12 +1,6 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.net.DatagramPacket;
-import java.net.InetAddress;
-import java.net.SocketAddress;
 import java.nio.ByteBuffer;
-
-
-// TODO: Make Elevator Input Packet an extension of DatagramPacket
 
 public class ElevatorInputPacket {
 	
@@ -14,11 +8,10 @@ public class ElevatorInputPacket {
 	private int floor;							// Floor where request originated from
 	private FloorButtonDirection floorButton;	// Button pressed on Floor (up or down)
 	private int carButton; 						// Floor Button pressed by passenger in elevator
-	private DatagramPacket packet;				// DatagramPacket for sending or receiving the ElevatorInputPacket information
 	
-	private static final DateTimeFormatter TIME_FORMAT = DateTimeFormatter.ofPattern("HH:mm:ss:SSS");	// Format for the timestamp. Uses a 24 hour clock (hour is 0-23)
-	private static final int BYTE_ARRAY_LENGTH = 28;
-	private static final int TIMESTAMP_STRING_LENGTH = 12;
+	private static final DateTimeFormatter TIME_FORMAT = DateTimeFormatter.ofPattern("HH:mm:ss:SSS");	// String format of the time stamp. Uses a 24 hour clock (hour is 0-23)
+	protected static final int BYTE_ARRAY_LENGTH = 28;	// Length of the byte array needed to transmit all information in an ElevatorInputPacket
+	private static final int TIMESTAMP_STRING_LENGTH = 12;	// TODO: remove this?
 	
 	private static final int HOURS_INDEX = 0;
 	private static final int MINUTES_INDEX = 4;
@@ -40,7 +33,6 @@ public class ElevatorInputPacket {
 		this.carButton = carButton;
 	}
 	
-	
 	// Constructor for ElevatorInputPacket where no timestamp is given
 	public ElevatorInputPacket(int floor, FloorButtonDirection floorButton, int carButton){
 		this.timeStamp = new TimeStamp(LocalDateTime.now().format(TIME_FORMAT));		// set timestamp to the current time
@@ -59,24 +51,6 @@ public class ElevatorInputPacket {
 		this.floorButton = FloorButtonDirection.values()[buf.getInt(FLOOR_BUTTON_INDEX)];		// get the value of the floor button direction from the bytebuffer
 		this.carButton = buf.getInt(CAR_BUTTON_INDEX);		// get the value of the car button from the ByteBuffer
 	}
-	
-	// Constructor for creating an ElevatorInputPacket from a DatagramPacket
-	public ElevatorInputPacket(DatagramPacket p){
-		this.packet = p;	// set the object's DatagramPacket as the given DatagramPacket
-		
-		ByteBuffer buf = ByteBuffer.allocate(BYTE_ARRAY_LENGTH);	// create a ByteBuffer to parse the data byte array
-		buf.put(this.packet.getData());		// put all bytes from the DatagramPacket data array into new buffer
-		
-		this.timeStamp = new TimeStamp(buf.getInt(HOURS_INDEX), buf.getInt(MINUTES_INDEX), buf.getInt(SECONDS_INDEX), buf.getInt(MILLISECONDS_INDEX));		// get time stamp from the buffer, reading each integer
-		this.floor = buf.getInt(FLOOR_INDEX);		// get floor number from the ByteBuffer
-		this.floorButton = FloorButtonDirection.values()[buf.getInt(FLOOR_BUTTON_INDEX)];		// get the value of the floor button direction from the bytebuffer
-		this.carButton = buf.getInt(CAR_BUTTON_INDEX);		// get the value of the car button from the ByteBuffer
-	}
-	
-	// Constructor for creating an ElevatorInputPacket to send
-	public ElevatorInputPacket(TimeStamp timeStamp, int floor, FloorButtonDirection floorButton, int carButton, InetAddress address, int port){
-		this.timeStamp = timeStamp;
-	}
 
 	public TimeStamp getTimeStamp() {
 		return this.timeStamp;
@@ -92,55 +66,6 @@ public class ElevatorInputPacket {
 
 	public int getCarButton() {
 		return this.carButton;
-	}
-	
-	
-	/*
-	 * Below are all the methods from the DatagramPacket class
-	 */
-	public InetAddress getAddress(){
-		return this.packet.getAddress();
-	}
-	
-	public byte[] getData(){
-		return this.packet.getData();
-	}
-	
-	public int getLength(){
-		return this.BYTE_ARRAY_LENGTH;
-	}
-	
-	// TODO: fix this function to return the actual offset
-	public int getOffset(){
-		return 1;
-	}
-	
-	public int getPort(){
-		return this.packet.getPort();
-	}
-	
-	public SocketAddress getSocketAddress(){
-		return this.packet.getSocketAddress();
-	}
-	
-	public void setAddress(InetAddress iaddr){
-		this.packet.setAddress(iaddr);
-	}
-	
-	public void setData(byte[] buf){
-		this.packet.setData(buf);
-	}
-	
-	public void setData(byte[] buf, int offset, int length){
-		this.packet.setData(buf, offset, length);
-	}
-	
-	public void setPort(int iport){
-		this.packet.setPort(iport);
-	}
-	
-	public void setSocketAddress(SocketAddress address){
-		this.packet.setSocketAddress(address);
 	}
 	
 	// TODO: add toString, hashCode, equals
@@ -175,13 +100,6 @@ public class ElevatorInputPacket {
 		System.out.println("Floor: " + this.getFloor());
 		System.out.println("Floor Button Direction: " + this.getFloorButton().toString());
 		System.out.println("Car Button: " + this.getCarButton());
-	}
-	
-	// Returns a DatagramPacket containing the data in this class. Packet does not contain an address!
-	public DatagramPacket getDatagramPacket(){
-		this.packet.setData(this.getBytes());
-		this.packet.setLength(BYTE_ARRAY_LENGTH);
-		return this.packet;
 	}
 	
 	public static void main(String args[]){
