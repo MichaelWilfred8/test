b84f910509cf27f4f14f6a1f10602a73e33c5a06
Rahul Patel <r.spatel2323@gmail.com>
2019-01-30 00:07:18 -0500

    Create and send data to scheduler class
    Floor now sends time, floor, direction and destination to port 23 for
    scheduler

--- Floor.java
+++ Floor.java
@@ -4,8 +4,6 @@
 
 import java.io.*;
 import java.net.*;
-import java.nio.ByteBuffer;
-import java.time.LocalDateTime;
 import java.util.Arrays;
 
 public class Floor {
@@ -18,6 +16,12 @@ public class Floor {
 	DatagramSocket sendReceiveSocket;
 
 	public Floor(Scheduler scheduler, int floorNumber){
+		try {
+			sendReceiveSocket = new DatagramSocket();
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+		
 		this.scheduler = scheduler;
 		this.floorNumber = floorNumber;
 
@@ -35,38 +39,42 @@ public class Floor {
 	}
 
 	/**
-	 * 
-	 * @param button The Button that has been pressed
-	 * @return Message to be sent to Scheduler
+	 * @param time The time request was initiated in bytes [Hours,Minutes,Seconds,Milliseconds]
+	 * @param direction The direction requester would like to travel and destination floor
+	 * @return byte array to be sent within a DatagramPacket via DatagramSocket
 	 */
-	public byte[] createMessage(FloorButton button) {
+	public byte[] createMessage(byte[] time, String[] direction) {
+		byte[] returnBytes=null;//instantiate array to be returned
+		int directionCode;//direction code
+		if(direction[0].equalsIgnoreCase("UP")) {//if requester wants to go up
+			directionCode = 1;
+		}else if(direction[0].equalsIgnoreCase("DOWN")) {//if requester wants to go down
+			directionCode = 0;
+		}else directionCode=-1;//invalid request, should never happen
 
-		FloorButtonDirection direction = button.getDirection();//TODO: add button.pressButton() to method that will call createMessage
-		LocalDateTime now = LocalDateTime.now();//time of request  
-
-		ByteArrayOutputStream output = new ByteArrayOutputStream();
-		output.write(now.getHour());
-		output.write(now.getMinute());
-		output.write(now.getSecond());
-		int nano = now.getNano();
-		ByteBuffer bb = ByteBuffer.allocate(4); 
-	    bb.putInt(nano);
-	    try {
-			output.write(bb.array());
+		ByteArrayOutputStream output = new ByteArrayOutputStream();//output can be dynamically written to
+		try {
+			for (int i=0;i<time.length;i++) {//write each time parameter
+				output.write(time[i]);
+			}
+			output.write(floorNumber);//write the floor number
+			if (directionCode!=-1) {//if the direction code is not invalid
+				output.write(directionCode);
+			}else {//else throw an exception
+				throw new Exception("Invalid Direction Code");
+			}
+			output.write(Integer.parseInt(direction[1]));//write destination floor
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		output.write(floorNumber);
-		output.write(direction.getValue());
-
-		byte[] returnBytes = output.toByteArray();
-		System.out.println(nano);
+		returnBytes = output.toByteArray();//creates single byte array to be sent
+		//System.out.println(Arrays.toString(returnBytes));
 		return returnBytes;
 	}
 
-	public void sendAndReceive(FloorButton button){
-		byte[] message = createMessage(button);
+	public void sendAndReceive(byte[] message){
 
 		// Construct a datagram packet that is to be sent to a specified port on a specified host.
 		try {
@@ -75,21 +83,28 @@ public class Floor {
 			e.printStackTrace();
 			System.exit(1);
 		}
-		
+
 		//print details of outgoing packet
-		System.out.println("Client: Sending packet:");
+		System.out.println("Floor: Sending packet:");
 		System.out.println("To host: " + sendPacket.getAddress());
 		System.out.println("Destination host port: " + sendPacket.getPort());
 		int len = sendPacket.getLength();
 		System.out.println("Length: " + len);
-		System.out.println("Containing: ");
+		System.out.print("Containing: ");
 		System.out.println("(Bytes)" + Arrays.toString(sendPacket.getData()) + "\n");
 	}
-	
+
+	/**
+	 * Create a request to be sent to the scheduler
+	 * @param request String containing request information (time, floor, direction, destination)
+	 */
 	public void newRequest(String[] request) {
-		TimeStamp ts = new TimeStamp (request[0]);
-		byte[][]time = ts.toBytes();
+		TimeStamp ts = new TimeStamp (request[0]);//create a new timestamp from the request string
+		byte[] time = ts.toBytes();//get the bytes of the timestamp
+		byte[] message = createMessage(time, Arrays.copyOfRange(request,2,4));
 		
+		sendAndReceive(message);
+
 	}
 
 	/**
