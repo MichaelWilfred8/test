f3228cd76b84a8d088da70f52b98626c79da3815
Rahul Patel <r.spatel2323@gmail.com>
2019-02-12 13:46:28 -0500

    Updated scheduler to test new FloorHandler

--- Scheduler.java
+++ Scheduler.java
@@ -450,19 +450,22 @@ public class Scheduler {
 
 	public static void main(String args[]) throws UnknownHostException, InterruptedException{
 		Scheduler s = new Scheduler();
-		//DataPacket p = new DataPacket(OriginType.SCHEDULER, (byte) s.carStatus.id, SubsystemType.FLOORLAMP, new byte[]{(byte) 4, Direction.UP.getByte()});
+		DataPacket p = new DataPacket(OriginType.SCHEDULER, (byte) s.carStatus.id, SubsystemType.FLOORLAMP, new byte[]{(byte) 4, Direction.UP.getByte()});
 
 		//DataPacket p = new DataPacket(OriginType.SCHEDULER, (byte) this.carStatus.id, SubsystemType.MOTOR, new byte[] {MotorState.UP.getByte()});
 
 		//System.out.println(p.toString());
-		//s.sendRequest(p, OriginType.ELEVATOR, (byte) s.carStatus.id);
+		
+		s.sendRequest(p, OriginType.FLOOR, (byte) s.carStatus.id);
+		
+		s.sendRequest(p, OriginType.FLOOR, (byte) s.carStatus.id);
 		
 //		s.carStatus.addFloor(4);
 //		System.out.println("Next destination = " + s.carStatus.getNextDestination());
 //		
-		s.startElevator();
-		s.continueMovingElevator();
-		s.stopElevator();
+		//s.startElevator();
+		//s.continueMovingElevator();
+		//s.stopElevator();
 		
 		//s.moveUpOneFloor();
 	}
